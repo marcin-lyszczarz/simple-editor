e15634fa7845653be425ee35990372d2eeb6201a
Marcin Łyszczarz <lyszczarz.marcin@gmail.com>
2020-11-16 21:24:11 +0100

    add delete button

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,13 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-const text = document.querySelector('.textarea');
+const text = document.querySelector('.textarea--js');
 const btnLoad = document.querySelector('.load--js');
 const btnSave = document.querySelector('.save--js');
+const btnDelete = document.querySelector('.delete--js');
 
-const entry = localStorage.getItem('entry');
+
+
 
 btnLoad.addEventListener('click', ()=>{
     text.value = localStorage.getItem('entry');
@@ -18,5 +20,19 @@ btnLoad.addEventListener('click', ()=>{
 
 btnSave.addEventListener('click', ()=>{
     localStorage.setItem('entry', text.value);
+    const currentValue = localStorage.getItem('entry');
     text.value = '';
+    if(currentValue){
+        document.querySelector('.info--js').innerHTML = '!!!';
+    }else{
+        document.querySelector('.info--js').innerHTML = '';
+    }
+});
+
+btnDelete.addEventListener('click', () => {
+    const currentValue = localStorage.getItem('entry');
+    if(currentValue){
+        localStorage.setItem('entry', text.value = '');
+        document.querySelector('.info--js').innerHTML = '';
+    }
 });
